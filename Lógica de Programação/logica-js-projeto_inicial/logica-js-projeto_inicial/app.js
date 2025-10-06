7304850a6dfa5b9bf03124e84414befb6117b8a7
Yuly Rauen <rauenyuly@gmail.com>
2025-10-06 19:41:33 -0300

    adicionando a quantidade de tentativas para descobrir o numero

--- Lógica de Programação/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ Lógica de Programação/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -9,17 +9,24 @@ console.log(numeroSecreto);
 //igual um alerta mas com caixa de texto para enviar resposta
 let chute;
 
+//inicia com 1 porque a pessoa precisa fazer pelo menos uma tentativa
+let tentativas = 1;
+
 //enquanto chute não for igual ao número secreto
 while (chute != numeroSecreto) {
   chute = prompt("Escolha um número entre 1 e 10");
   //realizando uma comparação, usando condição
   if (chute == numeroSecreto) {
-    alert(`Parabéns! Você descobriu o número secreto: ${numeroSecreto}`);
+    alert(
+      `Parabéns! Você descobriu o número secreto: ${numeroSecreto}, com ${tentativas} tentativas`
+    );
   } else {
     if (chute > numeroSecreto) {
       alert(`O número secreto é menor que ${chute}`);
     } else {
       alert(`O número secreto é maior que ${chute}`);
     }
+    //tentativas = tentativas + 1
+    tentativas++;
   }
 }
