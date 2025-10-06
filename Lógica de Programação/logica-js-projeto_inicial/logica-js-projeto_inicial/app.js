f3f4d37c505d493ecba873d234d7d5ac622ff19e
Yuly Rauen <rauenyuly@gmail.com>
2025-10-06 19:23:48 -0300

    concatenacao no js

--- Lógica de Programação/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ Lógica de Programação/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,12 +1,17 @@
 alert("Boas vindas ao jogo do número secreto!");
 
 //criando uma variável
-let numeroSecreto = 29;
+let numeroSecreto = 2;
+
+//deixando o numeroSecreto escondido
+console.log(numeroSecreto);
 
 //igual um alerta mas com caixa de texto para enviar resposta
 let chute = prompt("Escolha um número entre 1 e 10");
 
 //realizando uma comparação, usando condição
 if (chute == numeroSecreto) {
-  console.log("Parabéns! Você descobriu o número secreto.");
+  alert(`Parabéns! Você descobriu o número secreto: ${numeroSecreto}`);
+} else {
+  alert("Errou! Tente novamente.");
 }
