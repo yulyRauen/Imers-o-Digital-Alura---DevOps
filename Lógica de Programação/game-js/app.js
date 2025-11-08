c15ea97f0fd4d300c52f10e608b6b2914111dbc8
Yuly Rauen <rauenyuly@gmail.com>
2025-11-08 10:14:14 -0300

    Interagindo com o HTML

---
+++ Lógica de Programação/game-js/app.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector("h1");
+titulo.innerHTML = "Jogo do número secreto";
+
+let paragrafo = document.querySelector("p");
+paragrafo.innerHTML = "Escolha um número entre 1 e 10";
+
+//criando uma função para chamar no botão
+function verificarChute() {
+  console.log("o botão foi clicado");
+}
